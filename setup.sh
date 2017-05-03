b7dab4450f91235ca80576c48f5ea0a40ae97592
zkanda <hi@zkanda.io>
2017-05-03 11:28:32 +0800

    Updated setup in include neovim

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
-GH_HOME=$HOME/code/github.com
+CURRENT_DIR=$(realpath .)
+
+echo $CURRENT_DIR
 
 
 # Git
-ln -sf $GH_HOME/dotfiles/gitconfig $HOME/.gitconfig
-ln -sf $GH_HOME/dotfiles/gitignore_global $HOME/.gitignore_global
+ln -sf $CURRENT_DIR/gitconfig $HOME/.gitconfig
+ln -sf $CURRENT_DIR/gitignore_global $HOME/.gitignore_global
 
 # Tmux
-ln -sf $GH_HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $CURRENT_DIR/tmux.conf $HOME/.tmux.conf
+
+# Neovim
+mkdir -p ~/.config/nvim/
+ln -sf $CURRENT_DIR/init.vim $HOME/.config/nvim/init.vim
 
 # Vim
-ln -sf $GH_HOME/dotfiles/vimrc $HOME/.vimrc
+ln -sf $CURRENT_DIR/vimrc $HOME/.vimrc
 
 
-ln -sf $GH_HOME/dotfiles/inputrc $HOME/.inputrc
+ln -sf $CURRENT_DIR/inputrc $HOME/.inputrc
 
+# sshrc
+ln -sf $CURRENT_DIR/sshrc $HOME/.sshrc
+mkdir -p $HOME/.sshrc.d/
+ln -sf $CURRENT_DIR/vimrc $HOME/.sshrc.d/.vimrc
+ln -sf $CURRENT_DIR/tmux.conf $HOME/.sshrc.d/.tmux.conf
